8364a699f71c3841e078f314f2b5a7a8e7b44970
Keshav Purohit <201751021@iiitvadodara.ac.in>
2020-07-09 17:42:31 +0530

    Start -- adding a basic screen

---
+++ trans.py
@@ -0,0 +1,10 @@
+# अनुवादक
+
+from tkinter import *
+
+screen = Tk()
+screen.title('अनुवादक') #name of calculator
+screen.configure(bg = 'gray') #backgroud colour of calc
+screen.geometry('500x400')
+
+screen.mainloop()
